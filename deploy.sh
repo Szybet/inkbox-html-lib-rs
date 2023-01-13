8730577a4ae529c84cfa26efa4f525527d8be13a
Szybet <53944559+Szybet@users.noreply.github.com>
2023-01-13 17:43:11 +0100

    submodule one dir setup

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
+rm -rf lib
+mkdir lib
+mkdir lib/include
+
+cargo install cross
 ~/.cargo/bin/cross build --release --target armv7-unknown-linux-musleabihf
-~/.cargo/bin/cbindgen . -o libreader-rs.h
+mv target/armv7-unknown-linux-musleabihf/release/libreader_rs.a lib/
+
+cargo install cbindgen
+~/.cargo/bin/cbindgen . -o lib/include/libreader-rs.h
