fcb7cbdf1d3ba70a5299628bdd56efc05d8547ad
Szybet <53944559+Szybet@users.noreply.github.com>
2023-08-17 22:36:11 +0200

    make it work with cpp again

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-rm -rf lib
-mkdir lib
-mkdir lib/include
+#rm -rf lib
+#mkdir lib
+#mkdir lib/include
 
 cargo install cross
-~/.cargo/bin/cross build --release --target armv7-unknown-linux-musleabihf
+RUSTFLAGS='-C link-arg=-s' ~/.cargo/bin/cross build --release --target armv7-unknown-linux-musleabihf
 mv target/armv7-unknown-linux-musleabihf/release/libreader_rs.a lib/
-# If it's as a submodule
-cp lib/libreader_rs.a ../prebuild
 
 cargo install cbindgen
 ~/.cargo/bin/cbindgen . -o lib/include/libreader-rs.h
